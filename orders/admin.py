2421d2c89fc5793f7a181f5bff466311dcd14e7b
201220007 <85940970+201220007@users.noreply.github.com>
2022-04-16 21:51:05 +0800

    Update admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(MemberRecord)
+admin.site.register(VenueRecord)
+admin.site.register(Record)
+# Register your models here.
