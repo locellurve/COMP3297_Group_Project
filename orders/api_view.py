fc0bb69c3e07884fb4c53b4464c492a485b94a9f
201220007 <85940970+201220007@users.noreply.github.com>
2022-04-16 21:51:36 +0800

    Update api_view.py

--- orders/api_view.py
+++ orders/api_view.py
@@ -2,23 +2,27 @@ import json
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from orders.records import *
 
-
-@api_view(['GET','POST',])
+@api_view(['POST',])
 def index(request):
-    str1=request.data
-    str1=json.dumps(str1)
-    query=json.loads(str1)
+    str1 = request.data
+    str1 = json.dumps(str1)
+    query = json.loads(str1)
     if query['type']=='create':
         list=query['content']
         for i in list:
-            create_record(i['hkuid'],i['venue'],i['date'],i['time'])
+            recordid=create_record(i['hkuid'],i['venue'],i['datetime'])
+            return {"state":"success","cmd":"create","hkuid":i['hkuid'],"recordid":recordid,"venue":i['venue'],"datetime":i['datetime']}
     elif query['type']=='modify':
         list = query['content']
         for i in list:
-            modify_record(i['recordid'],i['hkuid'], i['venue'], i['date'], i['time'])
+            modify_record(i['recordid'],i['hkuid'], i['venue'], i['datetime'])
+            return {"state": "success", "cmd": "modify", "hkuid": i['hkuid'], "recordid": i["recordid"], "venue": i['venue'],
+                    "datetime": i['datetime']}
     elif query['type'] == 'delete':
         list = query['content']
         for i in list:
             delete_record(i['recordid'])
-    return Response({"msg":"succeed"})
+            return {"state": "success", "cmd": "delete","recordid": i["recordid"]}
+    return {"state":"fail"}
