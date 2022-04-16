3bf764970f06a3ea5b0fd8488f852a8572b045c4
201220007 <85940970+201220007@users.noreply.github.com>
2022-04-16 21:52:10 +0800

    Update models.py

--- orders/models.py
+++ orders/models.py
@@ -1,15 +1,24 @@
 from django.db import models
+from django.db import models
+
 
 # Create your models here.
 class MemberRecord(models.Model):
-    uid = models.CharField(max_length = 10)
-    name = models.CharField(max_length = 150)
+    uid = models.CharField(max_length=10)
+    name = models.CharField(max_length=150)
+
     def __str__(self):
         return self.UID
-    
-    
-class Record(models.model):
+
+class Record(models.Model):
     student = models.ForeignKey(
-        Student, on_delete=models.CASCADE)  # to be edited
+        MemberRecord, on_delete=models.CASCADE)  # to be edited
     venue = models.CharField(max_length=10)
     datetime = models.DateTimeField()
+# Create your models here.
+
+class VenueRecord(models.Model):
+    venue_code=models.CharField(max_length=20)
+    location=models.CharField(max_length=150)
+    type=models.CharField(max_length=2)
+    capacity=models.IntegerField()
